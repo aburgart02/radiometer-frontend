f1af3295ec042cddfd8ea62f322e81bc1ebc2d3c
a.burgart02 <a.burgart02@gmail.com>
2024-02-16 16:09:08 +0500

    Add calibration downloading

--- src/pages/calibrations/calibration/calibration.tsx
+++ src/pages/calibrations/calibration/calibration.tsx
@@ -13,6 +13,18 @@ function Calibration(): ReactElement {
     const calibrations = useAppSelector(getCalibrations);
     const calibration = calibrations.filter((calibration) => calibration.Id === calibrationId)[0];
 
+    const handleDownloadCalibration = () => {
+        const jsonData = new TextDecoder().decode(Uint8Array.from(atob(calibration.Data), c => c.charCodeAt(0)));
+        const blob = new Blob([jsonData], { type: 'application/json' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'data.json';
+        document.body.appendChild(a);
+        a.click();
+        URL.revokeObjectURL(url);
+    }
+
     return (
         <>
             <div className="detailed-page-container">
@@ -28,6 +40,7 @@ function Calibration(): ReactElement {
                     {calibration.Description}
                 </p>
             </div>
+            <button type="button" className="action-button" onClick={handleDownloadCalibration}>Скачать</button>
             <button type="button" className="action-button" onClick={() => {
                 browserHistory.back();
             }}
